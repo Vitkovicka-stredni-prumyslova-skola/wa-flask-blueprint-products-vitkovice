e26ec77101baed7458675d2db7dbc7a3112ef4c8
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-02-19 14:52:51 +0100

    create

--- products/products.py
+++ products/products.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, render_template
-from API.api import *
-
+from API.api import ReadAllProducts
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
 @products_bp.route('/products')
 def index():
+<<<<<<< HEAD
     data = GetAllProducts().json()
     #l = len(data)
     return render_template('products/products.html', products = data)
+=======
+    response = ReadAllProducts().json()
+    
+    return render_template('products/products.html', response=response)
+>>>>>>> parent of c2a9854 (create get all products)
