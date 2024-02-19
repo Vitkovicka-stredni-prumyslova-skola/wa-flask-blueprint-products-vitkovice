5ce427f8b0e949cd4fb2b651221d3f9d469d32a7
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-02-19 15:13:38 +0100

    update api and products

--- API/api.py
+++ API/api.py
@@ -1,31 +1,21 @@
 from flask import Blueprint, render_template
 import requests
-<<<<<<< HEAD
+
 import json
 
-=======
->>>>>>> parent of c2a9854 (create get all products)
+
 api_bp = Blueprint('api_bp', __name__,
     template_folder='templates',
     static_folder='static')
 URL_API = "https://fakestoreapi.com"
 
-<<<<<<< HEAD
 #Načte  seznam produktů z API v JSON formátu a vrátí jej jako pole.
 def GetAllProducts():   
     
     request = requests.get(f"{URL_API}/products")
     
-   # return json.loads(request.text)
-    return request
+    return json.loads(request.text)
+   
  
     
 
-=======
-def ReadAllProducts():
-    return requests.get(f"{URL_API}/products")
-    
-#@api_bp.route('/')
-#def index():
-#    return render_template('zatím nic')
->>>>>>> parent of c2a9854 (create get all products)
--- products/products.py
+++ products/products.py
@@ -1,17 +1,12 @@
 from flask import Blueprint, render_template
-from API.api import ReadAllProducts
+from API.api import GetAllProducts
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
 @products_bp.route('/products')
 def index():
-<<<<<<< HEAD
-    data = GetAllProducts().json()
-    #l = len(data)
-    return render_template('products/products.html', products = data)
-=======
-    response = ReadAllProducts().json()
+    data = GetAllProducts()
+    l = len(data)
     
-    return render_template('products/products.html', response=response)
->>>>>>> parent of c2a9854 (create get all products)
+    return render_template('products/products.html', length = l, products = data)
