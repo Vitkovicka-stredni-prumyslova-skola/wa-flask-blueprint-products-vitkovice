a84ff71dedbb511d1cbac00faed37fec827e9877
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-02-20 12:25:42 +0100

    create methods for single products

--- API/api.py
+++ API/api.py
@@ -15,7 +15,13 @@ def GetAllProducts():
     request = requests.get(f"{URL_API}/products")
     
     return json.loads(request.text)
-   
+
+#Načte  seznam produktů z API v JSON formátu a vrátí jej jako pole.
+def GetSingleProducts(id):   
+    
+    request = requests.get(f"{URL_API}/products/" + str(id))
+    
+    return json.loads(request.text)
  
     
 
--- products/products.py
+++ products/products.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from API.api import GetAllProducts
+from API.api import GetAllProducts, GetSingleProducts
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
@@ -10,3 +10,9 @@ def index():
     l = len(data)
     
     return render_template('products/products.html', length = l, products = data)
+
+@products_bp.route('/products/detail')
+def detailOfProduct():
+    data = GetSingleProducts(1)
+
+    return render_template('products/detail.html', detailOfPorduct = data)
